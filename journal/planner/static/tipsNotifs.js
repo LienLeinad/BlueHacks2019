973c667b25bc7bea0c9970cf0148f5d228199b37
dancerwinter <jessica.manzano@obf.ateneo.edu>
2019-02-24 04:49:13 +0800

    added date()

---
+++ journal/planner/static/tipsNotifs.js
@@ -0,0 +1,22 @@
+// store the text in a var
+var tipNotif = document.getElementByID('tip');
+var tip = document.getElementByID('tip-container');
+
+// store 
+
+// create time object
+var currentTime = new Date();
+
+// create minute object
+var currentMinute = currentTime.getDate();
+
+// while time != 1min, don show
+// show notif when 1min
+// hide notif after 1min
+
+
+
+function fn() {
+	alert(Date());
+}
+
