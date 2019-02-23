b9d8d29e7290e9dec835efdf0fa1dbe7bb3e4a1f
dancerwinter <jessica.manzano@obf.ateneo.edu>
2019-02-23 17:54:14 +0800

    Added add plan feature and modal shows panes

--- modals.js
+++ modals.js
@@ -1,15 +1,20 @@
 // Get the modal
 var modal = document.getElementById('myModal');
-
+var modal2 = document.getElementById('myModal2');
 // Get the button that opens the modal
 var plancard = document.getElementById("plan-card");
-
+var btn = document.getElementById("btn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
+var span2 = document.getElementsByClassName("close2")[0];
 // When the user clicks the button, open the modal 
 plancard.onclick = function() {
   modal.style.display = "block";
+}
+
+btn.onclick = function() {
+  modal.style.display = "none";
+  modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -17,9 +22,15 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+  else if(event.target == modal2){
+  	modal2.style.display = "none";
   }
 }
