fd7bebd697181a2855711818ef0ade101e78444b
LienLeinad <neil.bau@tista.net>
2019-02-23 22:35:11 +0800

    finished form integration for homepage, fixed forms for plans, adding plan_tag

--- journal/planner/models.py
+++ journal/planner/models.py
@@ -11,11 +11,29 @@ class Day(models.Model):
 
 # model to store plans within the day
 class Plans(models.Model):
+    PHYSICAL = 'Physical'
+    EMOTIONAL = 'Emotional'
+    SOCIAL = 'Social'
+    OCCUPATIONAL = 'Occupational'
+    INTELLECTUAL = 'Intellectual'
+    SPIRITUAL = 'Spiritual'
     day = models.ForeignKey(Day, on_delete = models.CASCADE)
     time_start = models.TimeField('Time Start')
     time_end = models.TimeField('Time End')
     plan_title = models.CharField(max_length = 20)
     plan_desc = models.TextField(max_length = 300)
+    TAG_CHOICES = (
+        (PHYSICAL, 'Physical'),
+        (EMOTIONAL, 'Emotional'),
+        (SOCIAL, 'Social'),
+        (OCCUPATIONAL, 'Occupational'),
+        (INTELLECTUAL, 'Intellectual'),
+        (SPIRITUAL,'Spiritual'),
+    )
+    plan_tag = models.CharField(
+        max_length = 10,
+        choices = TAG_CHOICES     
+    )
     def __str__(self):
         return self.plan_title
     
