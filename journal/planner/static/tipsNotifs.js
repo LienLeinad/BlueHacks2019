f38e7b0693b1609bdc62044582ac9c68fc45522c
LienLeinad <neil.bau@tista.net>
2019-02-24 06:36:56 +0800

    fixed error with tip Logic

--- journal/planner/static/tipsNotifs.js
+++ journal/planner/static/tipsNotifs.js
@@ -6,8 +6,8 @@ var h = currentTime.getHours();
 var m = currentTime.getMinutes();
 var s = currentTime.getSeconds();
 
-	document.getElementById("time").innerText = h + ":" + m + ":" + s;
-	document.getElementById("time").textContent = h + ":" + m + ":" + s;
+	document.getElementById("time-clock").innerText = h + ":" + m + ":" + s;
+	document.getElementById("time-clock").textContent = h + ":" + m + ":" + s;
 	setTimeout(setTime, 1000);
 }
 
